4f4ad319ab5a0b36b5003981c3f367257c84830b
mitep <milos.tepic@yahoo.com>
2018-03-04 12:39:50 +0100

    Predjeno sa jsp na rest i html

---
+++ projekat/src/main/resources/static/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+		$("div.pozoriste").click(function(){
+			 alert("klik pozorista");
+		});
+
+		$("div.bioskop").click(function(){
+ 			alert("klik bioskope");
+		});
+
+		$("button.login").click(function(){
+ 			alert("klik prijava");
+		});
+	});
