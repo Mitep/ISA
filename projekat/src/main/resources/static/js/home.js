23a2ff4f51cb9e3e72ba6723cb098f08f293ee78
mitep <milos.tepic@yahoo.com>
2018-03-04 15:51:14 +0100

    Popravljena html stranica

---
+++ projekat/src/main/resources/static/js/home.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("#home_div > div.pozoriste").click(function(){
+		 //prikazuje pozorista
+		alert("klik pozorista");
+	});
+	
+	$("#home_div > div.bioskop").click(function(){
+		//prikazuje bioskope
+		alert("klik bioskop");
+	});
+	
+});
